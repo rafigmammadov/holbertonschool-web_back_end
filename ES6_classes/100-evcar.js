a168bc8c87fae17cd6a21e6c8df1e94176a732a1
Rafig Mammadov <rafigmammadov@yahoo.com>
2024-07-21 17:37:03 +0000

    EVCar

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,14 +1,12 @@
 import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    return new Car();
   }
 }
