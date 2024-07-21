d165e956ad78fc709851958b3164c1b1e43f0f30
Rafig Mammadov <rafigmammadov@yahoo.com>
2024-07-21 11:14:27 +0000

    Iterating through report objects

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+
+  for (const items of Object.values(report.allEmployees)) {
+    for (const item of items) {
+      allEmployees.push(item);
+    }
+  }
+
+  return allEmployees;
+}
