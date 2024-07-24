adbb75031710ef53d2cef48ecddc70597b3da986
Rafig Mammadov <rafigmammadov@yahoo.com>
2024-07-24 16:11:58 +0000

    Basic list of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,21 @@
 export default function getListStudents() {
-  return [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-]
+  const student = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+  return student;
 }
+
