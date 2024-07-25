af2aaf41f1a1e3b29839f0b6918eb69408097403
Rafig <rafigmammadov@yahoo.com>
2024-07-25 15:42:44 +0000

    Set data structure

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,5 +1,3 @@
 export default function setFromArray(array) {
-	const bir_insan_ovladi_da_variable_vermir = new Set(array);
-
-	return bir_insan_ovladi_da_variable_vermir;
+  return new Set(array);
 }
