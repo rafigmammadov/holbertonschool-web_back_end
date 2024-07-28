fc302ca9333979d5cb87271439d273d912d9477d
Rafig Mammadov <131705272+rafigmammadov@users.noreply.github.com>
2024-07-28 12:39:43 +0100

    Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
