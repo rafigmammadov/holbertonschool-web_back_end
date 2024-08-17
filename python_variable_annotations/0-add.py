14ce81ec70d9f83abe09cd53e6f118572ed14e52
Rafig <rafigmammadov@yahoo.com>
2024-08-18 00:53:16 +0400

    Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module that contains add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that takes a float a and a float b
+    as arguments and returns their sum as a float.
+    """
+    return a + b
