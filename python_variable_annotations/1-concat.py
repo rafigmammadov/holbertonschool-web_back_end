3ed16691db2204ceb69dc25a7981ae09829ca92b
Rafig <rafigmammadov@yahoo.com>
2024-08-18 00:56:13 +0400

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that contains concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that takes a string str1 and a string str2
+    as arguments and returns a concatenated string
+    """
+    return str1 + str2
