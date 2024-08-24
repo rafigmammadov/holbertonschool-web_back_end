70c4de8f2121d90797510be429b7ef3b35166e0c
Rafig <rafigmammadov@yahoo.com>
2024-08-24 12:09:51 +0400

    Async Comprehensions

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that contains the coroutine async_comprehension
+"""
+import asyncio
+from typing import Iterable
+import random
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Iterable[float]:
+    """
+    The coroutine will collect 10 random numbers using
+    an async comprehensing over
+    async_generator, then return the 10 random numbers.
+    """
+    return [i async for i in async_generator()]
