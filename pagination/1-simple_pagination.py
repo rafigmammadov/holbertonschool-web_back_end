5bc1b22bc2a64b17439bb5931e6054c2512ccd12
Rafig <rafigmammadov@yahoo.com>
2024-08-25 11:56:33 +0400

    Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -28,8 +28,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0,
-        assert isinstance(page_size, int) and page_size > 0,
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
