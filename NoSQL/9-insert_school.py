55e703409ed8523f5a96a9906a6f77940df9250f
Rafig <rafigmammadov@yahoo.com>
2024-08-25 14:32:47 +0400

    Insert some data

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,5 +8,4 @@ def insert_school(mongo_collection, **kwargs):
     """ mongo_collection will be the pymongo collection object
         Returns the new _id
     """
-    document_id = mongo_collection.insert(kwargs)
-    return document_id
+    return mongo_collection.insert_one(kwargs).inserted_id
