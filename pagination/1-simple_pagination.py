f52314711295f9ca525cbc30805e91491400d147
Rafig <rafigmammadov@yahoo.com>
2024-08-25 11:52:48 +0400

    Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Module that contains Server class and
+implemented get_page method.
+"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Function that should return a tuple of size two containing
+        a start index and an end index corresponding to the range of indexes
+        to return in a list for those particular pagination parameters.
+        """
+        start_index = page_size * (page - 1)
+        end_page = start_index + page_size
+
+        return (start_index, end_page)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0,
+        assert isinstance(page_size, int) and page_size > 0,
+
+        dataset = self.dataset()
+        start_index, end_index = self.index_range(page, page_size)
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
